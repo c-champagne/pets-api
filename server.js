e11a0da0f5c885d35e6a2da264a32e1da17200f0
c-champagne <cassielchampagne@gmail.com>
2020-04-13 16:37:36 -0400

    Progress made on Pet API

--- server.js
+++ server.js
@@ -37,15 +37,56 @@ var owners = [
 
 
 // GET /api/owners
-
+app.get("/api/owners", function (req, res, next) {
+    res.send(owners);
+})
 // GET /api/owners/:id
-
+app.get("/api/owners/:id", function (req, res, next) {
+    let id = parseInt(req.params.id);
+    let foundId = owners.find (function (owner) {
+        return owner.id === id;
+    })
+    res.send(foundId);
+})
 // POST /api/owners
+app.post("/api/owners", function (req, res, next) {
+    let nextID = owners.reduce((acc,curr) => {
+        if(curr.id > acc){
+            return curr.id
+        }
+        return acc++
+    }, 0) + 1;
+    let newOwner = {
+        id: nextID,
+        name: req.body.owner,
+        pets: req.body.pets
+    }
+    owners.push(newOwner);
 
+    res.send(newOwner)
+})
 // PUT /api/owners/:id
-
+app.put("/api/owners/:id", function (req, res, next) {
+    let id = parseInt(req.params.id);
+    let foundID = owners.find (function (owner) {
+        return item.id === id;
+    });
+    foundID.name = req.body.name;
+})
 // DELETE /api/owners/:id
-
+app.delete("/api/owners/:id", function (req, res, next) {
+    let id = parseInt(req.params.id);
+    let removedOwner;
+    let foundID = owners.find (function (owner) {
+        return owner.id === id;
+    });
+    for (let i = 0; i <owners.length; i++) {
+        if (owners[i] === foundID) {
+            removedOwner = todoList.splice(i, 1);
+        }
+    }
+    res.send(removedOwner);
+})
 // GET /api/owners/:id/pets
 
 // GET /api/owners/:id/pets/:petId
